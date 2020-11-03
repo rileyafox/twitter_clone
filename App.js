272d2fd4f828566532d116681b05e33887d47d0e
rileyafox <41808064+rileyafox@users.noreply.github.com>
2020-11-03 16:49:29 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Widgets from "./Widgets";
+import './App.css';
+
+function App() {
+  return (
+    // BEM convention
+    <div className= "app">
+      <Sidebar />
+
+      <Feed />
+
+      {/* Build Widgets */}
+      <Widgets />
+    </div>
+
+  )
+}
+export default App;
